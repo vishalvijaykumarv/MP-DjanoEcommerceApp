d13b4248a9414cf61fbf8d160a46cc9972113138
vishal vijayakumar <vishalvijayakumarv@gmail.com>
2023-10-01 23:38:27 +0530

    updated with templates

--- test_one/hello_app/views.py
+++ test_one/hello_app/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from django.shortcuts import render
 
 def print_hello(request):
-    return HttpResponse("hello django")
+    movie_data = {'movie_list' : [
+        {
+        "movie_name":"movie_one",
+        "summary":"sample_name for a movie",
+        "year": 2023,
+        "success": True
+        },
+        {
+        "movie_name":"movie_two",
+        "summary":"",
+        "year": 2023,
+        "success": True
+        },
+        {
+        "movie_name":"movie_three",
+        "summary":"sample_name for a movie",
+        "year": 2023,
+        "success": True
+        },
+        {
+        "movie_name":"movie_four",
+        "summary":"sample_name for a movie",
+        "year": 2023,
+        "success": True
+        },
+        {
+        "movie_name":"movie_five",
+        "summary":"sample_name for a movie",
+        "year": 2023,
+        "success": True
+        },
+    ]
+}
+    return render(request,'sample.html',movie_data)
